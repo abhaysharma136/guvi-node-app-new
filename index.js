7020bb0968ae4f844921ae2e20c8dbc5102810d3
Abhay Sharma <abhaysharmajr@gmail.com>
2022-07-18 00:25:24 +0530

    delete and update movie

--- index.js
+++ index.js
@@ -27,12 +27,19 @@ app.get('/', function (request, response) {
 })
 
 app.get('/movies', async function (request, response) {
+  
+if(request.query.rating){
+  request.query.rating=+request.query.rating;
+}
+console.log(request.query);
+
   //db.movies.find({})
-  const movies= await client.db("guvi-new-db").collection("movies").find({}).toArray();
-    console.log(movies)
+  const movies= await client.db("guvi-new-db").collection("movies").find(request.query).toArray();
+    // console.log(movies)
     response.send(movies)
   })
 
+  //GEt Movie By ID
   app.get('/movies/:id',async function (request, response) {
     const {id}=request.params;
     console.log(request.params,id)
@@ -44,6 +51,8 @@ app.get('/movies', async function (request, response) {
     movie?response.send(movie):response.status(404).send({"msg":"Movie not found"})
   })
 
+
+//Create Movies
   app.post('/movies',async function (request, response) {
     const data=request.body;
     console.log(data);
@@ -51,6 +60,31 @@ app.get('/movies', async function (request, response) {
     const result=await client.db('guvi-new-db').collection('movies').insertMany(data);
     response.send(result)
   })
+//Delete Movie By ID
+app.delete('/movies/:id',async function (request, response) {
+    const {id}=request.params;
+    console.log(request.params,id)
+    // const movie=movies.find((mv)=>mv.id===id);
+    //db.movies.deleteone({id:101})
 
+    const result= await client.db("guvi-new-db").collection("movies").deleteOne({id:id})
+    console.log(result)
+    result.deletedCount>0
+    ?response.send({"msg":"Movie was succesfully Deleted"})
+    :response.status(404)
+    .send({"msg":"Movie not found"})
+  })
 
+  //Update Movie By ID
+  app.put('/movies/:id',async function (request, response) {
+    const {id}=request.params;
+    console.log(request.params,id)
+    const data=request.body;
+    // const movie=movies.find((mv)=>mv.id===id);
+    //db.movies.updateOne({id:101},{$set:data})
+
+    const result= await client.db("guvi-new-db").collection("movies").updateOne({id:id},{$set:data})
+    console.log(result)
+    response.send(result)
+  })
 app.listen(PORT,()=>console.log(`Node App Started in ${PORT}` ))
