29ec635ee879d1a224d96e80ee2e69024b0cceee
Abhay Sharma <abhaysharmajr@gmail.com>
2022-07-21 22:34:48 +0530

    id to _id

--- routes/helper.js
+++ routes/helper.js
@@ -1,17 +1,29 @@
 import { client } from '../index.js';
+import { ObjectId } from "mongodb";
 
 export async function updateMovieById(id, data) {
-    return await client.db("guvi-new-db").collection("movies").updateOne({ id: id }, { $set: data });
+    return await client.db("guvi-new-db")
+    .collection("movies")
+    .updateOne({ _id: ObjectId(id) }, { $set: data });
 }
 export async function deleteMovieById(id) {
-    return await client.db("guvi-new-db").collection("movies").deleteOne({ id: id });
+    return await client.db("guvi-new-db")
+    .collection("movies")
+    .deleteOne({ _id: ObjectId(id) });
 }
 export async function CreateMovies(data) {
-    return await client.db('guvi-new-db').collection('movies').insertMany(data);
+    return await client.db('guvi-new-db')
+    .collection('movies')
+    .insertMany(data);
 }
 export async function GetMovieById(id) {
-    return await client.db("guvi-new-db").collection("movies").findOne({ id: id });
+    return await client.db("guvi-new-db")
+    .collection("movies")
+    .findOne({ _id: ObjectId(id) });
 }
 export async function getAllMovies(request) {
-    return await client.db("guvi-new-db").collection("movies").find(request.query).toArray();
+    return await client.db("guvi-new-db")
+    .collection("movies")
+    .find(request.query)
+    .toArray();
 }
